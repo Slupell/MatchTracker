8af1451bafd4799e20bbf7fcc3f8323521b9ee3b
Slupell <dinaj248@gmail.com>
2025-03-07 19:00:00 +0400

    ...

--- src/components/MatchTracker/index.tsx
+++ src/components/MatchTracker/index.tsx
@@ -1,11 +1,40 @@
+import Image from "next/image";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const MatchTracker: React.FC = () => {
+  const [matches, setMatches] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      const response = await axios.get(
+        "https://app.ftoyd.com/fronttemp-service/fronttemp"
+      );
+      console.log(response);
+      return response.data;
+    };
+    fetchDate();
+  }, []);
+
   return (
     <div>
       <div>
-        <image href="\gettest\public\MatchTracker.svg" />
+        <Image
+          src="/MatchTracker.svg"
+          alt="MatchTracker"
+          width={271}
+          height={30}
+        />
         <span>
           Ошибка: Не удалось загрузить информацию <button>Обновить</button>
         </span>
+        <ul>
+          {matches.map((game, index) => (
+            <li key={index}>
+              <span>{game}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
