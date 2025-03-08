9452a59be624690f74c84b6ec4fe26b8033d87db
Slupell <dinaj248@gmail.com>
2025-03-08 16:32:24 +0400

    add styles

--- src/components/CommandBlocks/index.tsx
+++ src/components/CommandBlocks/index.tsx
@@ -1,5 +1,14 @@
-import { Matches } from "@/Types/ITypes";
+import { Matches, statusInGameRightNow } from "@/Types/ITypes";
 import React from "react";
+
+const statusMap: Record<
+  statusInGameRightNow,
+  { className: string; label: string }
+> = {
+  Scheduled: { className: "statusPreparing", label: "Preparing" },
+  Ongoing: { className: "statusLive", label: "Live" },
+  Finished: { className: "statusFinished", label: "Finished" },
+};
 
 export const CommandBlocks = ({ games }: { games: Matches[] | null }) => {
   if (!games) return <p>Нет данных</p>;
@@ -9,10 +18,12 @@ export const CommandBlocks = ({ games }: { games: Matches[] | null }) => {
         <ul key={index} className="commands">
           <div className="commandBlock">
             <div className="teams">{item.awayTeam.name}</div>
-            <div>
+            <div className="statusBar">
               {`${item.awayScore} : ${item.homeScore}`}
               <br />
-              <div className="status">{item.status}</div>
+              <div className={`status ${statusMap[item.status].className}`}>
+                {statusMap[item.status].label}
+              </div>
             </div>
             <div className="teams">{item.homeTeam.name}</div>
           </div>
